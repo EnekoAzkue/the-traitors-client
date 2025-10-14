80632c4766feccbe2b94c94fdd58bf5a3af9a289
Ignacio <ignayh2000@gmail.com>
2025-10-14 07:54:18 +0200

    feat(THET-24): Create basic structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,19 @@
 import { useState } from "react";
-import Home from "../components/screens/Home";
-import Login from "./screens/Login";
+// import Home from "../components/screens/Home";
+// import Login from "./screens/Login";
+import { View } from "react-native";
+import { SafeAreaFrameContext, SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native/";
+// import Login from "./Login";
 
 function App() {
 
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<any>(null);
   // const [] = useState();
 
   return (
     <>
-      {(!user) ?
-        <>
-          <Login />
-        </>
-        :
-        <>
-          <Home />
-        </>
-      }
+      <Text> TEST </Text>
     </>
   );
 }
