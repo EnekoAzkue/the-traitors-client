c52fb7c25c30daa23f25f010a512448d3639fe7c
Ignacio <ignayh2000@gmail.com>
2025-10-19 14:02:03 +0200

    feat(THET-24): Implement hardcoded signin

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -20,58 +20,81 @@ function App() {
 
   useEffect(() => {
     setTimeout(() => {
-      authClient();
+      authClient(false);
     }, 1000);
   }, []);
 
-  async function authClient() {
-    try {
-      await GoogleAuth.configure({
+  async function authClient(fixed: boolean) {
+    if (!fixed) {
+      getUser(); 
+    } else {
+      try {
+        await GoogleAuth.configure({
 
-        webClientId: ClientID.WEB,
-        scopes: [
-          GoogleAuthScopes.EMAIL,
-        ]
-      });
-      console.log(Logs.SUCCESSFUL_CONFIGURATION);
+          webClientId: ClientID.WEB,
+          scopes: [
+            GoogleAuthScopes.EMAIL,
+          ]
+        });
+        console.log(Logs.SUCCESSFUL_CONFIGURATION);
 
 
-      const currentUser: User | null = await GoogleAuth.getCurrentUser();
+        const currentUser: User | null = await GoogleAuth.getCurrentUser();
 
-      if (currentUser) {
-        console.log("user logged in: ", currentUser.name)
-        const idToken: string = await getIdToken();
-        console.log(idToken)
-        const authenticationAttempt: { statusCode: number, player: KaotikaPlayer | null } = await authenticatePlayer(ApiEndpoints.LOGGED_IN, idToken);
+        if (currentUser) {
+          console.log("user logged in: ", currentUser.name)
+          const idToken: string = await getIdToken();
+          console.log(idToken)
+          const authenticationAttempt: { statusCode: number, player: KaotikaPlayer | null } = await authenticatePlayer(ApiEndpoints.LOGGED_IN, idToken);
 
-        if (authenticationAttempt.statusCode === 200) {
-          setUser(authenticationAttempt.player);
-        } else {
-          await GoogleAuth.signOut();
-          setModalMessage(
-            ModalMessages.ERROR_USER_COULD_NOT_VERIFY);
-        }
-      };
+          if (authenticationAttempt.statusCode === 200) {
+            setUser(authenticationAttempt.player);
+          } else {
+            await GoogleAuth.signOut();
+            setModalMessage(
+              ModalMessages.ERROR_USER_COULD_NOT_VERIFY);
+          }
+        };
 
-      setInitialConf(true);
-    } catch (error) {
-      console.error(Logs.FAILED_CONFIGURATION, error);
+        setInitialConf(true);
+      } catch (error) {
+        console.error(Logs.FAILED_CONFIGURATION, error);
+      }
     }
   }
+
+    const userHandler = (newUser: KaotikaPlayer) => {
+    setUser(newUser);
+  }
+
+  const getUser = async () => {
+    const response = await fetch("https://kaotika-server.fly.dev/players/email/ignacio.ayaso@ikasle.aeg.eus");
+    console.log(response);
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+      if (data.status === "OK") {
+        console.log("");
+        userHandler({ ...data.data, rol: "ACOLYTE" });
+      }
+    }
+        setInitialConf(true);
+    
+  };
 
 
   async function getIdToken(): Promise<string> {
     let tokens = await GoogleAuth.getTokens();
-    
-    const tokenAlmostExpired : number = 300000;
+
+    const tokenAlmostExpired: number = 300000;
     let tokenExpirationMillisecs = 0;
     console.log(tokens)
-    if(tokens.expiresAt){
+    if (tokens.expiresAt) {
       tokenExpirationMillisecs = tokens.expiresAt - Date.now();
     }
 
     // Si el token ha expirado o va a expirar
-    const isTokenExpired : boolean = tokenExpirationMillisecs <= tokenAlmostExpired;
+    const isTokenExpired: boolean = tokenExpirationMillisecs <= tokenAlmostExpired;
 
     // if (isTokenExpired) {
     //   console.log("Is token expired? ", isTokenExpired);
