fd75b3af8302f4962e1787207231e79781883e31
Ignacio <ignayh2000@gmail.com>
2025-10-19 15:44:42 +0200

    feat(THET-24): Create Home Screen user rol management.

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -3,21 +3,36 @@ import ScreenContainer from "./ScreenContainer";
 import { Images, Roles } from "../../helpers/constants/constants";
 import React, { use, useContext } from "react";
 import { UserContext } from "../../helpers/contexts/contexts";
+import MortimerHome from "./role/mortimer/MortimerHome";
+import VillainHome from "./role/villain/VillainHome";
+import IstvanHome from "./role/istvan/IstvanHome";
+import AcolyteHome from "./role/acolyte/AcolyteHome";
 
 function Home() {
 
   const userContext = useContext(UserContext);
 
-  if(!userContext) throw new Error("User context is null at Home Component!!!");
+  if (!userContext) return <Text>User context is null at Home Component!!!"</Text>;
 
   const [user, setUser] = userContext;
 
 
-  return (
-    <ScreenContainer backgroundImg={Images.ACOLYTE_HOME}>
-      <Text style={{color:"white"}}>{user.rol}</Text>
-    </ScreenContainer>
-  );
+  switch (user.rol) {
+    case (Roles.MORTIMER):
+      return (<MortimerHome />);
+
+    case (Roles.VILLAIN):
+      return (<VillainHome />);
+
+    case (Roles.ISTVAN):
+      return (<IstvanHome/>);
+
+    case (Roles.ACOLYTE):
+      return (<AcolyteHome/>);
+
+    default: 
+      return (<Text>Error! Rol Not found</Text>)
+  }
 }
 
 
