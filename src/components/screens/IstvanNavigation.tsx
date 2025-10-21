e4908a0700072199089f4859ab92057d721f3c57
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2025-10-21 10:37:16 +0200

    feat(thet-68): make the filter to show different navigation depending on role

---
+++ src/components/screens/IstvanNavigation.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BlurView } from '@react-native-community/blur';
+import { createStaticNavigation } from '@react-navigation/native';
+import React from 'react';
+import { Images, Screens } from '../../helpers/constants/constants';
+import IstvanLab from './role/istvan/IstvanLab';
+import IstvanHome from './role/istvan/IstvanHome';
+import IstvanSettings from './role/istvan/IstvanSettings';
+
+const TabIcon = styled.Image`
+  position: relative;
+  top: 5px;
+  width: 35px;
+  height: 35px;
+  filter: brightness(${props => (props.$focused ? 150 : 100)}%)
+    grayscale(${props => (props.$focused ? 0 : 100)}%);
+`;
+
+const Navigator = createBottomTabNavigator({
+    screenOptions: ({ route }) => {
+        return {
+            tabBarShowLabel: false,
+            tabBarIcon: ({ focused }) => {
+                let tabIconSource;
+
+                switch (route.name) {
+                    case Screens.ISTVAN_HOME:
+                        tabIconSource = Images.HOME_ICON;
+                        break;
+
+                    case Screens.ISTVAN_SETTINGS:
+                        tabIconSource = Images.SETTINGS_ICON;
+                        break;
+
+                    case Screens.ISTVAN_LAB:
+                        tabIconSource = Images.LAB_ICON;
+                        break;
+                }
+
+                return <TabIcon source={tabIconSource} $focused={focused} />;
+            },
+            tabBarBackground: () => {
+                return (
+                    <BlurView
+                        blurAmount={1}
+                        overlayColor="rgba(0 0 0 / 0.1)"
+                        style={{ height: '100%' }}
+                    />
+                );
+            },
+            tabBarStyle: {
+                position: 'absolute',
+                overflow: 'hidden',
+                borderTopWidth: 0,
+                boxShadow: '0 -7.5px 5px rgba(255 255 255 / 0.1)',
+            },
+            headerShown: false,
+        };
+    },
+    screens: {
+        IstvanLab,
+        IstvanHome,
+        IstvanSettings,
+    },
+});
+
+const IstvanNavigation = createStaticNavigation(Navigator);
+
+export default IstvanNavigation;
